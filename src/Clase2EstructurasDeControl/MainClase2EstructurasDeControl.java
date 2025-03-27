2520716ff9ca04d8a929935e8684def917a28625
Cecilia <cecilia.delacamara@gmail.com>
2025-03-27 18:01:52 +0100

    Ejercicios 4,5 y 6 y Clase2 Estructuras de Control

---
+++ src/Clase2EstructurasDeControl/MainClase2EstructurasDeControl.java
@@ -0,0 +1,22 @@
+//package Clase2EstructurasDeControl;
+//import java.util.Scanner;
+//
+//
+//public class MainClase2EstructurasDeControl {
+//    public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    int numero;
+//
+//    while(true){
+//        System.out.println("Introduce un numero y pon 0 para salir");
+//        numero = sc.nextInt();
+//    if(numero == 0){
+//        //Si es cero, como pide el enunciado,salgo de bucle
+//        break;
+//    }
+//        System.out.println("Has introducido: " + numero);
+//    }
+//        System.out.println("Programa finalizado.");
+//    sc.close();
+//    }
+//}
--- src/ClaseConversionesDeTipo/MainClaseConversiones.java
+++ src/ClaseConversionesDeTipo/MainClaseConversiones.java
@@ -2,7 +2,7 @@ package ClaseConversionesDeTipo;
 
 public class MainClaseConversiones {
     public static void main(String[] args) {
-        //1. Conversion implicita de int a double
+//1. Conversion implicita de int a double
         // Declaramos una variable entera
 //        int numeroEntero = 25;
 //        double numeroDecimal = numeroEntero;
@@ -12,7 +12,7 @@ public class MainClaseConversiones {
 //    }
 //}
 
-        //2.Conversión explícita (double a int) con truncamiento
+//2.Conversión explícita (double a int) con truncamiento
         //Declaramos una variable double con un calor decimal
 
 //        double valorDecimal = 8.75;
@@ -27,9 +27,9 @@ public class MainClaseConversiones {
 //    }
 //}
 
-        //3. Desbordamiento al convertir int a byte
+//3. Desbordamiento al convertir int a byte
 
-    //Número mayor al rango de byte (-128 a 127)
+        //Número mayor al rango de byte (-128 a 127)
 //    int numeroGrande = 150;
 //
 //    //Conversión explícita a byte
@@ -44,4 +44,49 @@ public class MainClaseConversiones {
 //El número 150 excede el rango de byte (-128 a 127).
 //Java lo ajusta de forma cíclica, resultando en -106.
 
+//4. Conversión de char a int (Código ASCII)
+        //Declaramos un carácter
+//        char letra = 'C';
+//
+//        //Conversión implícita de char a int
+//        //Java convierte 'C' a su código ASCII (67)
+//
+//        int codigoASCII = letra;
+//
+//        System.out.println(letra);
+//        System.out.println(codigoASCII);
+//    }
+//}
+        //Cada carácter en Java tiene un código ASCII asociado.
+        //'A' se convierte en 65 automáticamente.
 
+//5. Conversión de String a int con Integer.parseInt()
+        //Declaramos una cadena con un número en formato texto
+
+//        String texto = "123";
+//
+//        //Convertimos la cadena a un número entero
+//
+//        int numero = Integer.parseInt(texto);
+//
+//        System.out.println(texto);
+//        System.out.println(numero);
+//        ;
+//    }
+//}
+
+//⚠ Precaución:
+// Si intentamos convertir "abc" a int, el programa lanzará un error.
+
+//6. Conversión de int a String
+
+//        int número = 226;
+//
+//        Convertimos el número a String
+//
+//        String texto = String.valueOf(número);
+//
+//        System.out.println(número);
+//        System.out.println(texto);
+//    }
+//}
