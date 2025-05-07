b308c3f0e467f9d3b68d9c5bb9d8dcd27d79190c
Cecilia <cecilia.delacamara@gmail.com>
2025-05-07 18:00:25 +0200

    ejercicio 2 con for

--- src/ClaseEstructurasDeDatos/ejercicio2.java
+++ src/ClaseEstructurasDeDatos/ejercicio2.java
@@ -8,13 +8,20 @@ import java.util.Arrays;
 public class ejercicio2 {
     public static void main(String[] args) {
 
-        int [] numeros = {2,6,20,3,32};
-        int suma = 0;
+        int[] numeros = {2, 6, 20, 3, 32};
+        int suma = 0; // variable para acumular la suma
 
         System.out.println("Los numeros del array son: " + Arrays.toString(numeros));
+//
+//        for (int num: numeros){
+//            suma += num; // suma = suma + num
+//        }
+//        System.out.println("La suma total de los valores del array es de: " + suma);
 
-        for (int num: numeros){
-            suma += num; // suma = suma + num
+
+        for (int i = 0; i < numeros.length; i++) {
+            suma += numeros[i];//Dentro del bucle, se usa el índice i para ir acumulando la suma
+            //System.out.println(numeros[i] + " ");
         }
         System.out.println("La suma total de los valores del array es de: " + suma);
     }
