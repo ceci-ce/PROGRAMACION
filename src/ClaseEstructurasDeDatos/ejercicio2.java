7cbbf470be5d653d99c2d99bee6b5a6426415ad5
Cecilia <cecilia.delacamara@gmail.com>
2025-05-07 17:45:14 +0200

    ejercicio 2 con foreach

--- src/ClaseEstructurasDeDatos/ejercicio2.java
+++ src/ClaseEstructurasDeDatos/ejercicio2.java
@@ -1,7 +1,21 @@
 package ClaseEstructurasDeDatos;
 
+import java.util.Arrays;
+
+//2. Sumar elementos
+//Suma todos los valores de un array de enteros y muestra el total.
+
 public class ejercicio2 {
     public static void main(String[] args) {
 
+        int [] numeros = {2,6,20,3,32};
+        int suma = 0;
+
+        System.out.println("Los numeros del array son: " + Arrays.toString(numeros));
+
+        for (int num: numeros){
+            suma += num; // suma = suma + num
+        }
+        System.out.println("La suma total de los valores del array es de: " + suma);
     }
 }
