7c6a79b90978a4ec5542be67a62d61d4e1b14b5e
Cecilia <cecilia.delacamara@gmail.com>
2025-05-14 17:20:14 +0200

    explicacion ejercicio 6

--- src/ClaseEstructurasDeDatos/ejercicio6.java
+++ src/ClaseEstructurasDeDatos/ejercicio6.java
@@ -23,3 +23,28 @@ public class ejercicio6 {
         }
     }
 }
+
+/* EXPLICACION
+- int[] → Indica que invertido es un array de enteros.
+- new int[...] → Usamos la palabra clave new para crear un nuevo array de tipo int.
+- original.length → Especifica el tamaño del nuevo array. Usamos la propiedad .length del array original para asegurarnos
+de que tengan la misma cantidad de elementos.
+
+int[] invertido = new int[5]; Se crea un array con 5 posiciones y todos los valores son 0 por defecto. Luego, el
+bucle for del ejercicio se encarga de reemplazar esos ceros con los números invertidos del array original.
+
+Se llena el array invertido desde el final del array original hacia el principio.
+
+original.length - 1 es el último índice del array. Se usa eso siempre para acceder al ultimo elemento del array
+
+Restamos i para ir desde el final al principio.
+
+Se muestran los numeros del array invertido con un for-each: "Para cada elemento num dentro del array invertido,
+ haz lo siguiente..."
+
+
+
+
+
+
+ */
