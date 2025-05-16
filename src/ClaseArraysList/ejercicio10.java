f611c95906d2dad02ced8641bc1f50b791b8edb5
Cecilia <cecilia.delacamara@gmail.com>
2025-05-16 22:09:39 +0200

    inicio ejercicio 10

---
+++ src/ClaseArraysList/ejercicio10.java
@@ -0,0 +1,20 @@
+package ClaseArraysList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//10. Eliminar duplicados de una lista
+//Crea una ArrayList con elementos repetidos (por ejemplo: [1, 2, 2, 3, 4, 4, 5])
+//Elimina los duplicados y muestra la lista final sin repetir.
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+
+        Integer[] array = {1, 2, 2, 3, 4, 4, 5};
+
+        ArrayList<Integer> elementos = new ArrayList<>(Arrays.asList(array));
+
+        int [] temporal = new int[elementos.lenght];
+
+    }
+}
