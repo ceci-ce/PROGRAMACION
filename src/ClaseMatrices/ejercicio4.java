8fad6489ee9b9ba2da3d662a001a51f6a385afc7
Cecilia <cecilia.delacamara@gmail.com>
2025-05-23 20:57:50 +0200

    ejercicio 4

--- src/ClaseMatrices/ejercicio4.java
+++ src/ClaseMatrices/ejercicio4.java
@@ -7,5 +7,23 @@ package ClaseMatrices;
 public class ejercicio4 {
     public static void main(String[] args) {
 
+        int[][] matriz5 = {
+                {2, 7, 0},
+                {1, 4, 6}
+        };
+
+        int[][] matrizTranspuesta = new int[3][2];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrizTranspuesta[i][j] = matriz5[j][i];
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matrizTranspuesta[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
