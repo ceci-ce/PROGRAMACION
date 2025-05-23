91dc3fb18b67dbd56cb22538b76974f7c922791b
Cecilia <cecilia.delacamara@gmail.com>
2025-05-23 18:06:08 +0200

    enunciado

---
+++ src/ClaseMatrices/ejercicio1.java
@@ -0,0 +1,9 @@
+package ClaseMatrices;
+//1. Suma de Dos Matrices:
+//Crea dos matrices de 3x3 y suma sus elementos.
+//Imprime el resultado de la suma.
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
