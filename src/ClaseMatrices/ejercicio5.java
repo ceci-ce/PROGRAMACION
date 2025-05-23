9f26c495c31d79c73394ffc514edd66af84feae6
Cecilia <cecilia.delacamara@gmail.com>
2025-05-23 20:35:25 +0200

    ejercicio 5

--- src/ClaseMatrices/ejercicio5.java
+++ src/ClaseMatrices/ejercicio5.java
@@ -7,5 +7,28 @@ package ClaseMatrices;
 public class ejercicio5 {
     public static void main(String[] args) {
 
+        int[][] matriz3 = {
+                {2, 6},
+                {3, 1}
+        };
+
+        int[][] matriz4 = {
+                {3, 0},
+                {7, 4}
+        };
+
+        int[][] matrizMultiplicacion = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrizMultiplicacion[i][j] = matriz3[i][j] * matriz4[i][j];
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matrizMultiplicacion[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
