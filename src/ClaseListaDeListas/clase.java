178be7e870834cfafd6fd8ab49ae00ea0698a27b
Cecilia <cecilia.delacamara@gmail.com>
2025-05-25 19:07:38 +0200

    clase de listas de listas

--- src/ClaseListaDeListas/clase.java
+++ src/ClaseListaDeListas/clase.java
@@ -1,4 +1,41 @@
 package ClaseListaDeListas;
 
+import java.util.ArrayList;
+
 public class clase {
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> estudiantes = new ArrayList<>();
+
+        ArrayList<String> estudiante1 = new ArrayList<>();
+        estudiante1.add("Juan");
+        estudiante1.add("9");
+        estudiante1.add("8");
+
+        ArrayList<String> estudiante2 = new ArrayList<>();
+        estudiante2.add("Raquel");
+        estudiante2.add("10");
+        estudiante2.add("6");
+        estudiante2.add("8");
+
+        estudiantes.add(estudiante1);
+        estudiantes.add(estudiante2);
+
+        for (ArrayList<String> estudiante: estudiantes){
+
+            int contador = 0;
+
+            for (String valor: estudiante){
+
+                if (contador == 0){
+                    System.out.print("Nombre: ");
+                } else {
+                    System.out.print("Nota" + contador + ": ");
+                }
+                System.out.print(valor + " ");
+                contador++;
+            }
+            System.out.println();
+        }
+    }
 }
