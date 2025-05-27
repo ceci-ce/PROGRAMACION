71fabaebf5bc419d66d88c9f4c7ec6765f7f94b2
Cecilia <cecilia.delacamara@gmail.com>
2025-05-27 19:41:53 +0200

    primer ejemplo funcion

---
+++ src/ClaseFunciones/clase.java
@@ -0,0 +1,15 @@
+package ClaseFunciones;
+
+public class clase {
+    public static void main(String[] args) {
+
+        saludar();
+    }
+
+    //1. Imprime un saludo
+    public static void saludar(){
+        System.out.println("Hola");
+    }
+
+
+}
