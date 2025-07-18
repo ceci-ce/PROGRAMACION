3d5e17dad15414068e4a9d8d0701544deeb6fd03
Cecilia <cecilia.delacamara@gmail.com>
2025-07-18 14:58:16 +0200

    Update claseb.java

--- src/ClaseFunciones/claseb.java
+++ src/ClaseFunciones/claseb.java
@@ -1,3 +1,24 @@
 package ClaseFunciones;
 
 public class claseb {
+  public static void main(String[] args){
+
+    String email = "pepe@gmail.com";
+
+        String nombreUsuario = email.substring(0, email.indexOf("@"));
+        System.out.println(nombreUsuario);
+
+        String servidor = email.substring(email.indexOf("@") + 1);
+        System.out.println(servidor);
+
+    }
+
+    public static double calcularMedia(int[] numeros){
+        int suma = 0;
+        for (int n: numeros){
+            suma += n;
+        }
+        return (double) suma / numeros.length;
+        }
+}
+
