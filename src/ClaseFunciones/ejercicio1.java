1a800a3c0e86a4b8251a6670820afd2741deca91
Cecilia <cecilia.delacamara@gmail.com>
2025-07-20 19:12:10 +0200

    ejercicio 1 funciones

--- src/ClaseFunciones/ejercicio1.java
+++ src/ClaseFunciones/ejercicio1.java
@@ -6,4 +6,15 @@ package ClaseFunciones;
 //int resultado = doble(4); // resultado = 8
 
 public class ejercicio1 {
-}
+    public static void main(String[] args) {
+
+        int resultadoDoble = doble(4);
+        System.out.println("El doble de 4 es " + resultadoDoble);
+    }
+
+        // funcion que calcula el doble de un numero
+
+        public static int doble(int numero){
+            return numero * 2;
+        }
+    }
