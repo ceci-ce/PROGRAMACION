6581367c68d887397f3b16280f4379735b10ab43
Cecilia <cecilia.delacamara@gmail.com>
2025-07-20 20:18:06 +0200

    ejercicio 5 con foreach

--- src/ClaseFunciones/ejercicio5.java
+++ src/ClaseFunciones/ejercicio5.java
@@ -16,8 +16,8 @@ public class ejercicio5 {
 
     public static int sumaEnteros(int[] array){
         int suma = 0;
-        for(int i = 0; i < array.length; i++){
-            suma += array[i];
+        for(int n: array){
+            suma += n;
         }
         return suma;
     }
