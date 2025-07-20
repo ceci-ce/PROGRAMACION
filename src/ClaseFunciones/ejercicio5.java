d168a926f929890d0235099ec8694f8e0e85f683
Cecilia <cecilia.delacamara@gmail.com>
2025-07-20 20:16:05 +0200

    ejercicio 5

---
+++ src/ClaseFunciones/ejercicio5.java
@@ -0,0 +1,24 @@
+package ClaseFunciones;
+
+//5. Función que sume los elementos de un array
+//Crea una función que reciba un array de enteros y devuelva la suma de sus elementos.
+//Ejemplo esperado:
+//int[] numeros = {4, 2, 7, 1};
+//int suma = sumarArray(numeros); // Resultado: 14
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+
+        int [] numeros = {3,6,9,1,4};
+        int suma = sumaEnteros(numeros);
+        System.out.println("La suma del array es: " + suma);
+    }
+
+    public static int sumaEnteros(int[] array){
+        int suma = 0;
+        for(int i = 0; i < array.length; i++){
+            suma += array[i];
+        }
+        return suma;
+    }
+}
