69d7400ed0e106e138b7a58ce0ede7d2424f1f61
Cecilia <cecilia.delacamara@gmail.com>
2025-07-22 18:20:35 +0200

    ejercicio 8

--- src/ClaseFunciones/ejercicio8.java
+++ src/ClaseFunciones/ejercicio8.java
@@ -1,10 +1,31 @@
 package ClaseFunciones;
 
+import java.util.Scanner;
+
 //8. Función que reciba dos enteros y devuelva el mayor
 //Llama a la función desde el main y muestra el resultado.
 
 public class ejercicio8 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dime el primer numero entero: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Dime el segundo numero entero: ");
+        int num2 = scanner.nextInt();
+
+        int mayor = obtenerMayor(num1,num2);
+        System.out.println("El numero mayor es: " + mayor);
+
+    }
+
+    public static int obtenerMayor(int n1, int n2){
+        if(n1 > n2){
+            return n1;
+        }else {
+            return n2;
+        }
     }
 }
