91190d0b3c2a5ebe2e5a7e1f7375fb836b655e63
Cecilia <cecilia.delacamara@gmail.com>
2025-07-22 18:50:25 +0200

    ejercicio 6

--- src/ClaseFunciones/ejercicio6.java
+++ src/ClaseFunciones/ejercicio6.java
@@ -1,5 +1,7 @@
 package ClaseFunciones;
 
+import java.util.Scanner;
+
 //6. Código para convertir en funciones
 //Este código calcula el área de un rectángulo.
 //Divídelo en dos funciones: una para leer datos y otra para calcular el área.
@@ -16,5 +18,29 @@ package ClaseFunciones;
 public class ejercicio6 {
     public static void main(String[] args) {
 
+        double [] datos = datosRectangulo();
+        double area = calcularArea(datos[0], datos[1]);
+        System.out.println("El area del rectangulo es: " + area);
+    }
+
+    // Funcion leer datos
+
+    public static double [] datosRectangulo(){
+
+        Scanner sc = new Scanner(System.in);
+        double datos [] = new double[2];
+
+        System.out.print("Introduce base: ");
+        datos[0] = sc.nextDouble();
+
+        System.out.print("Introduce altura: ");
+        datos[1] = sc.nextDouble();
+
+        return datos;
+    }
+    // Funcion calcular area
+
+    public static double calcularArea(double base, double altura){
+        return base * altura;
     }
 }
